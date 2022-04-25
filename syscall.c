3175833bbb39d6908393425766d927763fa0f795
Marcell Fulop <marcellfulop2000@gmail.com>
2022-04-24 17:46:29 -0700

    Removed unnecessary call

--- syscall.c
+++ syscall.c
@@ -127,8 +127,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_waitpid] sys_waitpid,
-[SYS_lab1_test] sys_lab1_test
+[SYS_waitpid] sys_waitpid
 };
 
 void
