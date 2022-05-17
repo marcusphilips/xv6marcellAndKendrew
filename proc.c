d1b8b1f2af4d5e34719c72e8206d59a3891f3f22
Marcell Fulop <marcellfulop2000@gmail.com>
2022-05-16 20:32:18 -0700

    bug fix

--- proc.c
+++ proc.c
@@ -337,7 +337,7 @@ scheduler(void)
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     // find the highest priority task
-    int lowest = 31;
+    int lowest = 32;
     struct proc* i; 
     for (i = ptable.proc; i < &ptable.proc[NPROC]; i++){
       // assuming RUNNABLE is equivalent to READY
@@ -362,24 +362,6 @@ scheduler(void)
         }
       }
     }
-    // for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-    //   if(p->state != RUNNABLE)
-    //     continue;
-
-    //   // Switch to chosen process.  It is the process's job
-    //   // to release ptable.lock and then reacquire it
-    //   // before jumping back to us.
-    //   c->proc = p;
-    //   switchuvm(p);
-    //   p->state = RUNNING;
-
-    //   swtch(&(c->scheduler), p->context);
-    //   switchkvm();
-
-    //   // Process is done running for now.
-    //   // It should have changed its p->state before coming back.
-    //   c->proc = 0;
-    // }
     release(&ptable.lock);
 
   }
